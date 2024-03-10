e82dc85eb8666ff90a6a3965c5a5313ba97de297
elia itlay <itlayelia@gmail.com>
2024-03-10 11:08:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tugas3_20411019/Forgot_Passsword.kt
@@ -0,0 +1,11 @@
+package com.example.tugas3_20411019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Forgot_Passsword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_passsword)
+    }
+}
---
+++ app/src/main/java/com/example/tugas3_20411019/Register_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.tugas3_20411019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Register_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
